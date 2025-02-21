b22d85c240ab23c0249427fae3bfa7901069e64e
Catherine <144566141+catherinetweeks@users.noreply.github.com>
2025-02-21 13:19:21 -0500

    Create lzdecode1.c
    part 1

---
+++ lzdecode1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void lzdecode1(char* dictionary, char* data, int dataLength) {
+    for (int i = 0; i < dataLength; i++) {
+        printf("%c", dictionary[data[i]]);
+    };
+};
